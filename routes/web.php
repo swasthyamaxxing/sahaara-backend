4182a373f51c7a1eb5263b7d9838e845c387e3e8
Atith Adhikari <atithadhikari5@gmail.com>
2026-08-07 15:02:11 +0545

    Updated landing page of API

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,10 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return response()->json([
+        'service'       => 'SaharaSewa API',
+        'status'        => 'operational',
+        'version'       => '1.0.0',
+        // 'documentation' => 'https://xyz.com/docs',
+    ]);
 });
